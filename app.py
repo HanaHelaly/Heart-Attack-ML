908b55e4d89870752990c9791b814c0eb47bdaf9
Hana <124832927+HanaHelaly@users.noreply.github.com>
2025-01-15 12:06:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from dash import Dash
 import dash_bootstrap_components as dbc
-
-# meta_tags are required for the app layout to be mobile responsive
+# Intiliazing Dash app
 app = Dash(__name__, suppress_callback_exceptions=True,
                 external_stylesheets=[dbc.themes.UNITED, dbc.icons.BOOTSTRAP])
 server = app.server
